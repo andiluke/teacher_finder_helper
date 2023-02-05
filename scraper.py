7f4a0723ab3a456a6aabf5ea7369ec608f7f672f
Andi Luke <andiluke@gmail.com>
2023-02-04 17:58:10 -0800

    POC done. just scraping one page & thinking about what I want to do

--- scraper.py
+++ scraper.py
@@ -1,12 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import urllib.parse
 
 url = "https://www.pcc.edu/schedule/default.cfm?fa=dspTopicDetails&topicid=ART"
 page = requests.get(url)
 
 soup = BeautifulSoup(page.content, "html.parser")
 course_list = soup.select("dl.course-list")
-course_links = course_list[0].find_all("a", limit=1)
+course_links = course_list[0].find_all("a", limit=3) # limit to avoid a ton of traffic while figuring this out
 print(course_links)
 print(type(course_links))
 print(len(course_links))
@@ -25,13 +26,17 @@ for course in course_links:
         if (link.get("href").startswith("https://www.pcc.edu/staff/directory/")):
             print(link.get("href")) # bio page url
             print(link.get_text()) # teacher name
+            print(f"https://google.com/search?q={urllib.parse.quote(link.get_text())}")
+            print(f"https://www.etsy.com/search_results_people.php?search_query={urllib.parse.quote(link.get_text())}")
+       
 
     
-    
+        # de duping would be nice
         # save instructor info
         # save instroctor + class info
 
-
+# save this to some kind of db (pickle? google sheets?)
+# oo structure with overrides for each site
 
 
 
