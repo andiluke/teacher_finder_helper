1aff38344a6fa278695bb21522c497d3fead1e64
Andi Luke <andiluke@gmail.com>
2024-02-24 12:54:13 -0800

    modified file

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import urllib.parse
+from dbtest import *
 
 
 class Teacher_Scraper:
@@ -8,6 +9,8 @@ class Teacher_Scraper:
     def __init__(self, listing_url, school_name):
         self.listing_url = listing_url
         self.school_name = school_name
+        self.db = get_database()
+        self.collection = self.db['teachers']
         print(f"Scraping teachers for school: {self.school_name}")
         print(f"url: {self.listing_url}")
 
@@ -27,24 +30,91 @@ class Teacher_Scraper:
             for link in session_links:
                 # find each instructor (there are other links in here)
                 if (link.get("href").startswith("https://www.pcc.edu/staff/directory/")): # markup specific selector
-                    print(link.get("href")) # bio page url
-                    print(link.get_text()) # teacher name
-                    print(f"https://google.com/search?q={urllib.parse.quote(link.get_text())}")
-                    print(f"https://www.etsy.com/search_results_people.php?search_query={urllib.parse.quote(link.get_text())}")
+                    #print(f"https://google.com/search?q={urllib.parse.quote(link.get_text())}")
+                    #print(f"https://www.etsy.com/search_results_people.php?search_query={urllib.parse.quote(link.get_text())}")
+                    data_to_store = {
+                        'teacher_name': link.get_text(),
+                        'school_name': self.school_name,
+                        'teacher_page': link.get("href"),
+                        'teacher_email': 'email goes here',
+                        'course_name': course_name
+                    }
+                    self.save_data(data_to_store)
+
+    def save_data(self, data):
+        print(data['teacher_name'])
+
+        # add the teacher
+        self.collection.update_one(
+            {
+                'teacher_name': data['teacher_name']
+            },
+            {
+                '$set': {
+                    'teacher_name': data['teacher_name']
+                }
+            },
+        
+            upsert=True
+        )
+        # add the school info
+        self.collection.update_one(
+            {
+                'teacher_name': data['teacher_name']
+            },
+            {
+                '$addToSet' : {
+                    'listings.$[]': { 
+                        'school_name': data['school_name'],
+                        'teacher_page': data['teacher_page'],
+                        'teacher_email': data['teacher_email'],
+                    }
+                }
+            },
+            #{
+            #    arrayFilters: [
+            #        {}
+            #    ]
+            #},
+            upsert=True
+        )
+        ## add the course info
+        #self.collection.update_one(
+        #    {
+        #        'teacher_name': data['teacher_name'],
+        #        'listings.school_name': data['school_name']
+        #    },
+        #    {
+        #        '$addToSet': {
+        #            'listings.$.courses': {
+        #                'course_name': data['course_name']
+        #            }
+        #        }
+        #    },
+        #    upsert=True)                
+
+    def clear_records(self):
+        print("Delete all records")
+        self.collection.delete_many({})
 
     def print_course_listings(self):    
         print(f"Found {self.course_links} course listings")
         print(self.course_links)
 
+    def print_db_listings(self):
+        for listing in self.collection.find():
+            print(listing)
 
 
 if __name__ == "__main__": 
     url = "https://www.pcc.edu/schedule/default.cfm?fa=dspTopicDetails&topicid=ART"
-    school_name = "Portland Community College"
+    school_name = "PDX Community College"
     scraper = Teacher_Scraper(url, school_name)
+    scraper.clear_records()
     scraper.get_course_listings()
     scraper.print_course_listings()
     scraper.get_info_from_listings()
+    scraper.print_db_listings()
    
 
     
@@ -65,7 +135,7 @@ output json
             school : "name of school",
             bio_page : "teachers bio page at this school",
             teacher_email : "teacher@scholl.com",
-            classes : [
+            courses : [
                 {
                     class_name: "name of class",
                     class_description: "description of class",
